d99dd73135fc4d4fe5e58892f02e00193f01ecee
Tanner Langley <tlangley141@gmail.com>
2014-05-19 22:48:46 -0600

    Adds basic edit function

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,23 @@
-var startTime, endTime, startPress,
+var startTime, endTime, startPress, currentText,
 	myButton = document.getElementById('myButton'),
+	$myButton = $(myButton),
 	display = $("#display"),
 	pressDuration = 1000;
 
-myButton.addEventListener('touchstart',function(event) { 
-	display.show();
-	startTime = new Date().getTime(); 
+	var editFields = $(".editField");
 
-	// Visual feedback... 
-    display.text("Hold...");
+myButton.addEventListener('touchstart',function(event) { 	
+
+	// a reference for our timer
+	startTime = new Date().getTime(); 
 
     // Start a timer for how long the button has been pressed
     startPress = window.setTimeout(pressHold, pressDuration);
+
+    // Visual feedback... 
+    display.show();
+    display.text("Hold...");
+
 },false);
 
 myButton.addEventListener('touchmove',function(event) {
@@ -19,18 +25,44 @@ myButton.addEventListener('touchmove',function(event) {
 	// clear The timer if the user moves their finger
   	window.clearTimeout(startPress);  
   	display.text("");	
+
 },false);
 
 myButton.addEventListener('touchend',function(event) {
+
 	endTime = new Date().getTime();
+	
+	// If the button hasn't been pressed long enough, we clear the timer
 	if (endTime < startTime + 800) {
     	window.clearTimeout(startPress);  
+    	// $myButton.text(parseInt($myButton.text()) + 1);
     	display.text("");		
     } else {
+    	// Fancy fadeout...
     	display.fadeOut();
     }
 },false);
 
 function pressHold() {
-	display.text("Press hold event!");	
+
+	currentText = $myButton.text();
+	$myButton.html('<input id="someField" class="editField" type="number" autofocus="true" value="' + currentText + '">');
+
+	// myButton.unbind("touchstart");
+	display.text("Press hold event!");
+
+	$(".editField").blur(function () {
+		currentText = $(this).val();
+		$(this).parent().html(currentText);
+	});
+	$(".editField").keyup(function (e) {
+		if (e.keyCode == 13) {		
+			currentText = $(this).val();
+			$(this).parent().html(currentText);
+		}
+	});
 }
+
+
+
+
